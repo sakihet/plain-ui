b658d1c338c80c694f6ed67cc4aa65576f7e2bf7
saki <sakihet@gmail.com>
2025-03-25 01:53:07 +0900

    update

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 import './style.css'
 
+// function
+function toggleTheme() {
+  if (document.firstElementChild?.classList.contains('dark')) {
+    document.firstElementChild?.classList.remove('dark')
+  } else {
+    document.firstElementChild?.classList.add('dark')
+  }
+}
 function openDialog() {
   document.querySelector('#dl').showModal()
 }
-
 function closeDialog() {
   document.querySelector('#dl').close()
 }
 
+// addEventListener
+document.querySelector('#toggleThemeButton').addEventListener("click", toggleTheme)
 document.querySelector('#dialogOpenButton').addEventListener("click", openDialog)
 document.querySelector('#dialogCloseButton').addEventListener("click", closeDialog)
