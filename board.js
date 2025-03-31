0840458f86d17180ccc408f59ed8e1f43d46e66e
saki <sakihet@gmail.com>
2025-04-01 01:08:32 +0900

    update

---
+++ board.js
@@ -0,0 +1,11 @@
+// function
+function toggleTheme() {
+  if (document.firstElementChild?.classList.contains('dark')) {
+    document.firstElementChild?.classList.remove('dark')
+  } else {
+    document.firstElementChild?.classList.add('dark')
+  }
+}
+
+// addEventListener
+document.querySelector('#toggleThemeButton').addEventListener("click", toggleTheme)
